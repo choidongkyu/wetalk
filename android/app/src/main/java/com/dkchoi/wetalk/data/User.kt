6901798f1813bab4c126077bb57ba0144cc43b47
dkchoi <ehdqkwk92@gmail.com>
2021-06-14 23:02:08 +0900

    주석 수정

--- android/app/src/main/java/com/dkchoi/wetalk/data/User.kt
+++ android/app/src/main/java/com/dkchoi/wetalk/data/User.kt
@@ -9,7 +9,7 @@ data class User(
     @PrimaryKey
     var id: String, // 유저 번호
     @field:Json(name = "profile_image")
-    var profileImage: String?, // 유저 프로필 사진
+    var profileImage: String?, // 유저 프로필 캐시 키
     @field:Json(name = "profile_text")
     var profileText: String?, // 유저 상태 메시지
     var name: String // 유저 이름
